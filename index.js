7fcb4be7e8159c60d30369a9c4cb91b66b0fc8b5
Max Lamb <lordmaxlamb@gmail.com>
2020-05-06 11:26:30 -0700

    more stuff

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const express = require('express');
+const cors = require('cors');
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -8,8 +11,8 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.json({ hello: 'world'});
+    res.json({ hello: 'world' });
 });
 
-app.listen(PORT, () => console.log(`running on port ${POST}`))
+app.listen(PORT, () => console.log(`running on port ${PORT}`));
 
